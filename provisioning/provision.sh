ecaded887bd21bab7856157e1b3f9184daec2fce
Magnus <vsasmry@Magnuss-MacBook-Pro.local>
2016-05-31 10:13:50 +0200

    One more error in provisioning script fixed  - if to fi

--- provisioning/provision.sh
+++ provisioning/provision.sh
@@ -2,7 +2,7 @@ echo "Installing Apache and setting up link to /vagrant"
 
 if [ -f "/var/vagrant_provision" ] then
   exit 0
-if
+fi
 
 apt-get update >/dev/null 2>&1
 apt-get install -y apache2 >/dev/null 2>&1
